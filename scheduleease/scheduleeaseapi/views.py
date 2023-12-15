39e432bf0cb1740c62d57726e2e9d4dfe6c8f9c1
Ujjwal Bhansali <ujjwalbhansali55@gmail.com>
2023-12-15 22:24:26 +0530

    Disscussion box compleated and interconnection of django to react

--- scheduleease/scheduleeaseapi/views.py
+++ scheduleease/scheduleeaseapi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import json
+
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -11,5 +11,17 @@ def listofreciver(request):
     return Response([{'firstname' :'ujjwa', 'email':'mudit@gmail.com'},{'firstname' :'ujjwal1', 'email':'mudit@gmail.com'},{'firstname' :'ujjwa', 'email':'mudit@gmail.com'},{'firstname' :'ujjwal', 'email':'mudit@gmail.com'},])
 
 @api_view(['post'])
-def activeuser(request):
+def activereciveruser(request):
     return Response([{'firstname' :'ujjwaactive', 'email':'mudit@gmail.com'}])
+
+# store a single message into the database and send if the message is stored or not
+@api_view(['post'])
+def messagesendertoreciver(request):
+    return Response(True)
+
+# give all the messages related to a perticular person
+@api_view(['post'])
+def messagesofauser(request):
+   
+    return Response( [{'sendertype':0,'messagetxt':"how are you"},{'sendertype':1,'messagetxt':"i am fine"}])
+
