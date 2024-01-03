01bafc310b40b7eaf620719441f32881c16c79a7
Ujjwal Bhansali <ujjwalbhansali55@gmail.com>
2024-01-03 13:38:24 +0530

    calander added

--- scheduleeasefrontend/src/App.js
+++ scheduleeasefrontend/src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import SideNavbar from './components/sideNavbar/sideNavbar';
-
 import Testcontainer from './components/testcontainer/testcontainer';
 import Manage_team from './components/manage_team/Manage_team'
 import Project from './components/Projects/Projects';
 import Tasklist from './components/Tasks/Tasklist';
-
-
 import Notification from './components/Notification/Notification';
 import Modal from './components/modal_form/Modal';
 import Cal from './components/calendar/Cal';
 import FileUpload from './components/file_upload/FileUpload';
-
-
 import DisscussionBox from './components/Disscussion/Disscussion'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
