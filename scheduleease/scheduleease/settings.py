48f5af5aacc9be47a0100e07af89bc35d5cd7aa8
karanpanwar8595 <vikupanwar2000@gmail.com>
2024-01-10 12:21:45 +0530

    commiting to merge

--- scheduleease/scheduleease/settings.py
+++ scheduleease/scheduleease/settings.py
@@ -86,13 +86,24 @@ WSGI_APPLICATION = 'scheduleease.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'college',
+        'USER': 'root',
+        'PASSWORD': 'Karan@123',
+        'HOST': 'localhost',
+        'PORT': '3306',  # Change it to your MySQL port if necessary
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
